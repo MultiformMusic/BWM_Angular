2b56a6180c7f8f34d3478a1ebed282afb26615c6
michel.dio33@gmail.com <michel.dio33@gmail.com>
2019-02-28 08:57:01 +0100

    api key remove

--- src/app/common/map/map.module.ts
+++ src/app/common/map/map.module.ts
@@ -5,14 +5,17 @@ import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
 import { MapService } from './map.service';
 import { CamelizePipe } from 'ngx-pipes';
+import { constants } from '../config/constants';
+
+
 
 @NgModule({
   declarations: [
     MapComponent
   ],
   imports:[
-    AgmCoreModule.forRoot({
-        apiKey: 'AIzaSyASxKk2jUYQi3s2KpznQ-Nf45P-p-QK6Kg'
+  AgmCoreModule.forRoot({
+        apiKey: constants.API_MAP_KEY
       }),
       CommonModule
   ],
